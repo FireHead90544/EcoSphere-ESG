6ca9149b520f4c7b81dd93017f043c1ec6e12fbc
M0neySSH <bishtmanish9456@gmail.com>
2026-07-12 17:29:23 +0530

    chore: add shadcn ui dependencies and global toaster

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono, Oxanium, Montserrat } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/sonner"
 import { cn } from "@/lib/utils";
 
 const montserratHeading = Montserrat({subsets:['latin'],variable:'--font-heading'});
@@ -26,6 +27,7 @@ export default function RootLayout({
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
+        <Toaster richColors position="top-right" />
       </body>
     </html>
   )
